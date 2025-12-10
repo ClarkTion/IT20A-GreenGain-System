681b3fd41addaf284e3bd25f377f5154f74baf0c
ElizhaSegura <elizhaalthea@gmail.com>
2025-12-10 23:20:47 +0800

    Added cards

---
+++ NetBeansProjects/GreenGain/src/GreenGain/Card.java
@@ -0,0 +1,116 @@
+
+package GreenGain;
+
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+public class Card extends javax.swing.JPanel {
+    
+    public Color getColor1(){
+        return color1;
+    }
+    public void setColor1(Color color1){
+        this.color1 = color1;
+    }
+    public Color getColor2(){
+        return color2;
+    }
+    public void setColor2(Color color2){
+        this.color2 = color2;
+    }
+    
+    private Color color1;
+    private Color color2;
+
+    
+    public Card() {
+        initComponents();
+        setOpaque(false);
+        color1 = Color.BLACK;
+        color2 = Color.WHITE;
+    }
+    
+    public void setData(Model_Card data){
+//        lbIcon.setIcon(data.getIcon());
+          lbTitle.setText(data.getTitle());
+          lbValues.setText(data.getValues());
+          lbDescription.setText(data.getDescription());
+    }
+        
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lbIcon = new javax.swing.JLabel();
+        lbDescription = new javax.swing.JLabel();
+        lbValues = new javax.swing.JLabel();
+        lbTitle = new javax.swing.JLabel();
+
+        lbIcon.setText("Icon");
+
+        lbDescription.setForeground(new java.awt.Color(255, 255, 255));
+        lbDescription.setText("Description");
+
+        lbValues.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lbValues.setForeground(new java.awt.Color(255, 255, 255));
+        lbValues.setText("Values");
+
+        lbTitle.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lbTitle.setForeground(new java.awt.Color(255, 255, 255));
+        lbTitle.setText("Title");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lbTitle)
+                    .addComponent(lbDescription)
+                    .addComponent(lbValues)
+                    .addComponent(lbIcon))
+                .addContainerGap(258, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addComponent(lbIcon, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(lbTitle)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lbValues)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
+                .addComponent(lbDescription)
+                .addGap(26, 26, 26))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+@Override
+public void paintComponents(Graphics grphcs){
+    Graphics2D g2=(Graphics2D)grphcs;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        GradientPaint g = new GradientPaint(0, 0, color1, 0, getHeight(), color2);
+        g2.setPaint(g);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+        g2.setColor(new Color(255, 255, 255, 50));
+        g2.setColor(Color.GREEN);
+        g2.fillOval(getWidth()- (getHeight() / 2), 10, getHeight(), getHeight());
+        g2.fillOval(getWidth()- (getHeight() / 2) - 20,  getHeight()/2+20, getHeight(), getHeight());
+        super.paintComponent(grphcs);
+}
+        
+        
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel lbDescription;
+    private javax.swing.JLabel lbIcon;
+    private javax.swing.JLabel lbTitle;
+    private javax.swing.JLabel lbValues;
+    // End of variables declaration//GEN-END:variables
+}
--- NetBeansProjects/GreenGain/src/GreenGain/Dashboard.java
+++ NetBeansProjects/GreenGain/src/GreenGain/Dashboard.java
@@ -21,6 +21,8 @@ public class Dashboard extends javax.swing.JFrame {
         panelBorder2 = new GreenGain.PanelBorder();
         menu1 = new GreenGain.Menu();
         header2 = new GreenGain.Header();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        form_Home1 = new GreenGain.Form_Home();
 
         javax.swing.GroupLayout jLayeredPane1Layout = new javax.swing.GroupLayout(jLayeredPane1);
         jLayeredPane1.setLayout(jLayeredPane1Layout);
@@ -39,6 +41,10 @@ public class Dashboard extends javax.swing.JFrame {
 
         header2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
 
+        jScrollPane1.setBackground(new java.awt.Color(216, 216, 216));
+        jScrollPane1.setBorder(null);
+        jScrollPane1.setViewportView(form_Home1);
+
         javax.swing.GroupLayout panelBorder2Layout = new javax.swing.GroupLayout(panelBorder2);
         panelBorder2.setLayout(panelBorder2Layout);
         panelBorder2Layout.setHorizontalGroup(
@@ -46,15 +52,18 @@ public class Dashboard extends javax.swing.JFrame {
             .addGroup(panelBorder2Layout.createSequentialGroup()
                 .addComponent(menu1, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
-                .addComponent(header2, javax.swing.GroupLayout.PREFERRED_SIZE, 696, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addGroup(panelBorder2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(header2, javax.swing.GroupLayout.PREFERRED_SIZE, 696, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane1))
+                .addGap(0, 0, 0))
         );
         panelBorder2Layout.setVerticalGroup(
             panelBorder2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(menu1, javax.swing.GroupLayout.DEFAULT_SIZE, 503, Short.MAX_VALUE)
+            .addComponent(menu1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(panelBorder2Layout.createSequentialGroup()
                 .addComponent(header2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 478, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -106,8 +115,10 @@ public class Dashboard extends javax.swing.JFrame {
     }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private GreenGain.Form_Home form_Home1;
     private GreenGain.Header header2;
     private javax.swing.JLayeredPane jLayeredPane1;
+    private javax.swing.JScrollPane jScrollPane1;
     private GreenGain.Menu menu1;
     private GreenGain.PanelBorder panelBorder2;
     // End of variables declaration//GEN-END:variables
